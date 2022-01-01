060f424f04d1e61f1063ab0281f2bdcefaa46bad
Maulidhiansyah Bayu Setiawan <62776223+masbayou@users.noreply.github.com>
2022-01-01 19:20:37 +0700

    Delete PraktikumJavaScript directory

--- PraktikumJavaScript/PraktikumJsSoal2/PraktikumJsSoal2.js
+++
@@ -1,5 +0,0 @@
-function konversi(){
-    let a = Number(document.getElementById("dollar1").value);
-    let b = a * 14.650
-    let c = document.getElementById("rupiah").value=b;
-}
--- PraktikumJavaScript/PraktikumJsSoal4/PraktikumjsSoal4.js
+++
@@ -1,6 +0,0 @@
-function hitung() {
-    let a = Number(document.getElementById("bil1").value);
-    let b = Number(document.getElementById("bil2").value);
-    let c = a + b;
-    document.getElementById("bill").value=c;
-}
